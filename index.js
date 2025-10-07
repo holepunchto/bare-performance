5c4f82063ead1aab22573fac92efe4c7b1d3aea8
yasser <dev@yasser.email>
2025-10-07 09:23:56 -0300

    Performance Timeline specs implementation (#5)

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { observerType } = require('./lib/constants')
+const { InvalidModificationError } = require('./lib/errors')
 const binding = require('./binding')
 
 const { TIME_ORIGIN } = binding
@@ -54,3 +56,313 @@ class PerformanceNodeTiming {
 
 // For Node.js compatibility
 exports.nodeTiming = new PerformanceNodeTiming()
+
+// Performance Timeline globals
+const globalObservers = new Set()
+const globalPendingObservers = new Set()
+let globalBuffer = []
+let pending = false
+
+class PerformanceEntry {
+  constructor(name, type, start, duration) {
+    this._name = name
+    this._type = type
+    this._start = start
+    this._duration = duration
+  }
+
+  get name() {
+    return this._name
+  }
+
+  get entryType() {
+    return this._type
+  }
+
+  get startTime() {
+    return this._start
+  }
+
+  get duration() {
+    return this._duration
+  }
+}
+
+exports.PerformanceEntry = PerformanceEntry
+
+class PerformanceMark extends PerformanceEntry {
+  constructor(name, opts = {}) {
+    const { startTime = exports.now(), detail = null } = opts
+
+    super(name, 'mark', startTime, 0)
+
+    this._detail = detail
+  }
+
+  get detail() {
+    return this._detail
+  }
+}
+
+exports.PerformanceMark = PerformanceMark
+
+class PerformanceMeasure extends PerformanceEntry {
+  constructor(name, startTime, duration, opts = {}) {
+    const { detail = null } = opts
+
+    super(name, 'measure', startTime, duration)
+
+    this._detail = detail
+  }
+
+  get detail() {
+    return this._detail
+  }
+}
+
+exports.PerformanceMeasure = PerformanceMeasure
+
+class PerformanceObserverEntryList {
+  constructor(entryList) {
+    this._list = entryList
+  }
+
+  getEntries() {
+    return this._list
+  }
+
+  getEntriesByType(type) {
+    return this._list.filter((entry) => entry.entryType === type)
+  }
+
+  getEntriesByName(name) {
+    return this._list.filter((entry) => entry.name === name)
+  }
+}
+
+exports.PerformanceObserverEntryList = PerformanceObserverEntryList
+
+class PerformanceObserver {
+  constructor(cb) {
+    this._entryTypes = new Set()
+    this._type = observerType.UNDEFINED
+    this._buffer = []
+    this._cb = cb
+  }
+
+  static get supportedEntryTypes() {
+    return ['mark', 'measure']
+  }
+
+  observe(opts = {}) {
+    if ((!opts.entryTypes && !opts.type) || (opts.entryTypes && opts.type)) {
+      throw new TypeError('opts.entryTypes OR opts.type must be specified')
+    }
+
+    if (
+      (this._type === observerType.MULTIPLE && opts.type) ||
+      (this._type === observerType.SINGLE && opts.entryTypes)
+    ) {
+      throw new InvalidModificationError(
+        'Cannot change the PerformanceObserver type'
+      )
+    }
+
+    if (this._type === observerType.UNDEFINED) {
+      this._type = opts.entryTypes ? observerType.MULTIPLE : observerType.SINGLE
+    }
+
+    this._entryTypes.clear()
+
+    if (this._type === observerType.MULTIPLE) {
+      for (const entryType of opts.entryTypes) {
+        if (PerformanceObserver.supportedEntryTypes.includes(entryType)) {
+          this._entryTypes.add(entryType)
+        }
+      }
+    } else {
+      if (PerformanceObserver.supportedEntryTypes.includes(opts.type)) {
+        this._entryTypes.add(opts.type)
+
+        if (opts.buffered === true) {
+          const bufferedEntries = globalBuffer.filter(
+            (entry) => entry.entryType === opts.type
+          )
+
+          if (bufferedEntries.length > 0) {
+            this._buffer.push(...bufferedEntries)
+
+            globalPendingObservers.add(this)
+            processPendingObservers()
+          }
+        }
+      }
+    }
+
+    if (this._entryTypes.size > 0) {
+      globalObservers.add(this)
+    } else {
+      this.disconnect()
+    }
+  }
+
+  takeRecords() {
+    const buf = this._buffer
+    this._buffer = []
+    return buf
+  }
+
+  disconnect() {
+    globalObservers.delete(this)
+    this._entryTypes.clear()
+    this._type = observerType.UNDEFINED
+  }
+}
+
+exports.PerformanceObserver = PerformanceObserver
+
+exports.mark = function mark(name, opts) {
+  const mark = new PerformanceMark(name, opts)
+
+  processEntry(mark)
+
+  globalBuffer.push(mark)
+
+  return mark
+}
+
+exports.clearMarks = function clearMarks(name) {
+  if (name) {
+    globalBuffer = globalBuffer.filter(
+      (entry) => entry.name !== name && entry.entryType !== 'mark'
+    )
+  } else {
+    globalBuffer = globalBuffer.filter((entry) => entry.entryType !== 'mark')
+  }
+}
+
+exports.measure = function measure(name, start, end) {
+  let opts = {}
+
+  if (
+    typeof start === 'object' &&
+    start !== null &&
+    Object.keys(start).length > 0
+  ) {
+    opts = start
+
+    if (!opts.start && !opts.end) {
+      throw new TypeError('opts.start or opts.end must be specified')
+    }
+
+    if (opts.name && opts.end && opts.duration) {
+      throw new TypeError(
+        'One of opts.name, opts.end or opts.duration must not be specified'
+      )
+    }
+
+    start = opts.start
+    end = opts.end
+  }
+
+  let endTime
+
+  if (end) {
+    endTime = toTimestamp(end)
+  } else if (start && opts.duration) {
+    endTime = toTimestamp(start) + toTimestamp(duration)
+  } else {
+    endTime = exports.now()
+  }
+
+  let startTime
+
+  if (start) {
+    startTime = toTimestamp(start)
+  } else if (end && opts.duration) {
+    startTime = toTimestamp(end) - toTimestamp(duration)
+  } else {
+    startTime = 0
+  }
+
+  const duration = endTime - startTime
+
+  const measure = new PerformanceMeasure(name, startTime, duration, opts)
+
+  processEntry(measure)
+
+  globalBuffer.push(measure)
+
+  return measure
+}
+
+exports.clearMeasures = function clearMeasures(name) {
+  if (name) {
+    globalBuffer = globalBuffer.filter(
+      (entry) => entry.name !== name && entry.entryType !== 'measure'
+    )
+  } else {
+    globalBuffer = globalBuffer.filter((entry) => entry.entryType !== 'measure')
+  }
+}
+
+exports.getEntries = function getEntries() {
+  return new PerformanceObserverEntryList(globalBuffer).getEntries()
+}
+
+exports.getEntriesByName = function getEntriesByName(name) {
+  return new PerformanceObserverEntryList(globalBuffer).getEntriesByName(name)
+}
+
+exports.getEntriesByType = function getEntriesByType(type) {
+  return new PerformanceObserverEntryList(globalBuffer).getEntriesByType(type)
+}
+
+// https://w3c.github.io/performance-timeline/#queue-a-performanceentry
+function processEntry(entry) {
+  for (const observer of globalObservers) {
+    if (observer._entryTypes.has(entry.entryType)) {
+      observer._buffer.push(entry)
+
+      globalPendingObservers.add(observer)
+      processPendingObservers()
+    }
+  }
+}
+
+// https://w3c.github.io/performance-timeline/#queue-the-performanceobserver-task
+function processPendingObservers() {
+  if (pending) return
+
+  pending = true
+  setImmediate(() => {
+    pending = false
+
+    const observers = Array.from(globalPendingObservers.values())
+    globalPendingObservers.clear()
+
+    for (const observer of observers) {
+      observer._cb(
+        new exports.PerformanceObserverEntryList(observer.takeRecords()),
+        observer
+      )
+    }
+  })
+}
+
+// https://w3c.github.io/user-timing/#convert-a-mark-to-a-timestamp
+function toTimestamp(mark) {
+  if (typeof mark === 'string') {
+    const performanceMark = globalBuffer.findLast(
+      (entry) => entry.name === mark && entry.entryType === 'mark'
+    )
+
+    if (performanceMark === undefined) {
+      throw new SyntaxError(`Mark ${mark} not found`)
+    }
+
+    return performanceMark.startTime
+  } else if (typeof mark === 'number') {
+    return mark
+  }
+}
--- test.js
+++ test.js
@@ -47,3 +47,125 @@ test('eventLoopUtilization', (t) => {
     }, 5)
   }, 5)
 })
+
+test('mark + observe', async (t) => {
+  const subtest = t.test((sub) => {
+    sub.plan(11)
+
+    let count = 0
+
+    performance.mark('ignored - early to the party')
+
+    const obs = new performance.PerformanceObserver((list, observer) => {
+      const entries = list.getEntries()
+
+      if (count === 0) {
+        sub.is(entries.length, 3)
+
+        sub.is(entries[0].name, 'test - 1')
+        sub.is(entries[1].name, 'test - 2')
+        sub.is(entries[2].name, 'test - 3')
+
+        sub.is(entries[0].startTime < entries[1].startTime, true)
+        sub.is(entries[1].startTime < entries[2].startTime, true)
+      } else if (count === 1) {
+        sub.is(entries.length, 1)
+
+        sub.is(entries[0].name, 'test - 4')
+        sub.is(entries[0].entryType, 'mark')
+        sub.is(entries[0].duration, 0)
+        sub.is(entries[0].detail, 42)
+
+        observer.disconnect()
+      } else {
+        sub.fail()
+      }
+
+      count++
+    })
+
+    obs.observe({ entryTypes: ['mark'] })
+
+    for (let n = 1; n < 4; n++) performance.mark(`test - ${n}`)
+
+    setTimeout(() => performance.mark('test - 4', { detail: 42 }), 100)
+
+    setTimeout(() => performance.mark('unseen test'), 200)
+  })
+
+  await subtest
+
+  performance.clearMarks()
+})
+
+test('observe - buffered option', (t) => {
+  t.plan(3)
+
+  performance.mark('first')
+
+  const obs = new performance.PerformanceObserver((list, observer) => {
+    const entries = list.getEntries()
+
+    t.is(entries.length, 2)
+    t.is(entries[0].name, 'first')
+    t.is(entries[1].name, 'second')
+
+    observer.disconnect()
+    performance.clearMarks()
+  })
+
+  performance.mark('second')
+
+  obs.observe({ type: 'mark', buffered: true })
+})
+
+test('measure', (t) => {
+  t.plan(3)
+
+  performance.mark('start')
+
+  setTimeout(() => {
+    performance.mark('end')
+
+    const measure = performance.measure('my-measure', 'start', 'end')
+
+    t.is(measure.name, 'my-measure')
+    t.is(measure.entryType, 'measure')
+    t.ok(measure.duration > 95 && measure.duration < 115)
+
+    performance.clearMarks()
+    performance.clearMeasures()
+  }, 100)
+})
+
+test('measure - options object', (t) => {
+  t.plan(1)
+
+  performance.mark('my-mark')
+  const measure = performance.measure('my-measure', {
+    start: 'my-mark',
+    detail: 42
+  })
+
+  t.is(measure.detail, 42)
+
+  performance.clearMarks()
+  performance.clearMeasures()
+})
+
+test('clearMarks + clear Measures', (t) => {
+  t.plan(5)
+
+  performance.mark('my-test')
+  performance.measure('my-measure', 'my-test')
+
+  t.is(performance.getEntries().length, 2)
+  t.is(performance.getEntriesByName('my-test').length, 1)
+  t.is(performance.getEntriesByType('mark').length, 1)
+  t.is(performance.getEntriesByType('measure').length, 1)
+
+  performance.clearMarks()
+  performance.clearMeasures()
+
+  t.is(performance.getEntries().length, 0)
+})
